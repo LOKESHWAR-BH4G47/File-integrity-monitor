254b2b83d1f86bd704ee2f89f151d8e4b75bdfc1
LOKESHWAR <128904339+LOKESHWAR-BH4G47@users.noreply.github.com>
2023-10-21 02:54:37 +0530

    Update file_integrity_monitor.py

--- file_integrity_monitor.py
+++ file_integrity_monitor.py
@@ -10,7 +10,7 @@ def calculate_file_hash(file_path, algorithm='sha512'):
             hash_algorithm.update(chunk)
     return hash_algorithm.hexdigest()
 
-# Function to erase the hash_file file if it already exists
+# Function to erase the hash_file if it already exists
 def erase_hash_file_if_already_exists():
     if os.path.exists('hash_file.txt'):
         os.remove('hash_file.txt')
@@ -29,22 +29,22 @@ if response == "A":
     erase_hash_file_if_already_exists()
 
     # Calculate hash from the target files and store in hash_file.txt
-    # Collect all files in the target folder
+    # Collect all files in the target folder (make sure the directory name matches)
     files = [f for f in os.listdir('./FILES') if os.path.isfile(os.path.join('./FILES', f))]
 
     # For each file, calculate the hash, and write to hash_file.txt
-    with open('hash_file.txt', 'w') as baseline_file:
+    with open('hash_file.txt', 'w') as hash_file:
         for file_name in files:
-            file_path = os.path.join('./Files', file_name)
+            file_path = os.path.join('./FILES', file_name)  # Make sure the directory name matches
             file_hash = calculate_file_hash(file_path)
-            baseline_file.write(f"{file_path}|{file_hash}\n")
+            hash_file.write(f"{file_path}|{file_hash}\n")
 
 elif response == "B":
     file_hash_dictionary = {}
 
     # Load file|hash from hash_file.txt and store them in a dictionary
-    with open('hash_file.txt', 'r') as baseline_file:
-        for line in baseline_file:
+    with open('hash_file.txt', 'r') as hash_file:
+        for line in hash_file:
             file_path, file_hash = line.strip().split('|')
             file_hash_dictionary[file_path] = file_hash
 
@@ -52,10 +52,11 @@ elif response == "B":
     while True:
         time.sleep(1)
 
+        # Collect all files in the target folder (make sure the directory name matches)
         files = [f for f in os.listdir('./FILES') if os.path.isfile(os.path.join('./FILES', f))]
 
         for file_name in files:
-            file_path = os.path.join('./FILES', file_name)
+            file_path = os.path.join('./FILES', file_name)  # Make sure the directory name matches
             file_hash = calculate_file_hash(file_path)
 
             # Notify if a new file has been created
@@ -66,7 +67,7 @@ elif response == "B":
             elif file_hash_dictionary[file_path] != file_hash:
                 print(f"{file_path} has changed!")
 
+        # Check if any files have been deleted
         for key in list(file_hash_dictionary.keys()):
             if not os.path.exists(key):
                 print(f"{key} has been deleted!")
-
